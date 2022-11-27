5b5f5abf771b54f8e1f9c6ad36a7f23e0fdd9c44
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-11-27 20:26:44 +0100

    [AN-2] Change text id

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		TextView textView = findViewById(R.id.hello_api);
+		TextView textView = findViewById(R.id.main_text_view);
 
 		var client = ApiClient.getInstance();
 
